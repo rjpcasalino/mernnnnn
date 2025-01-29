a30020ad1a47504722526a778e2e8548f967919f
rjpc <ryan@rjpc.net>
2025-01-28 22:17:40 -0800

    checkpoint; get nextjs somewhat styled and font awesome somewhat working...what a nightmare

--- TP/mern-app/my-next-app/app/page.tsx
+++ TP/mern-app/my-next-app/app/page.tsx
@@ -1,7 +1,68 @@
-'use client'
-import { useState, useEffect } from 'react';
+/* eslint-disable react/jsx-no-target-blank */
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-import './styles.css'
+import IndexNavbar from "/root/my-next-app/app/components/Navbars/IndexNavbar.js";
+import Footer from "/root/my-next-app/app/components/Footers/Footer.js";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsDown, faCheck } from '@fortawesome/free-solid-svg-icons'
+
+import { useState, useEffect } from "react";
+
+import "./styles.css";
+
+function Index() {
+  return (
+    <>
+      <FontAwesomeIcon icon={faThumbsDown} className="fa-fw" />
+      <IndexNavbar fixed />
+
+
+        <div className="container mx-auto">
+          <div className="flex flex-wrap justify-center bg-white shadow-xl rounded-lg -mt-64 py-16 px-12 relative z-10">
+            <div className="w-full text-center lg:w-8/12">
+              <p className="text-4xl text-center">
+                <span role="img" aria-label="love">
+                  😍
+                </span>
+              </p>
+              <h3 className="font-semibold text-3xl">
+                Do you love this Starter Kit?
+              </h3>
+              <p className="text-blueGray-500 text-lg leading-relaxed mt-4 mb-4">
+                Cause if you do, it can be yours now. Hit the buttons below to
+                navigate to get the Free version for your next project. Build a
+                new web app or give an old project a new look!
+              </p>
+              <div className="sm:block flex flex-col mt-10">
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus?ref=nnjs-index"
+                  target="_blank"
+                  className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-2 bg-blueGray-400 active:bg-blueGray-500 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                >
+                  Get started
+                </a>
+                <a
+                  href="https://github.com/creativetimofficial/notus-nextjs?ref=nnjs-index"
+                  target="_blank"
+                  className="github-star sm:ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg"
+                >
+                  <i className="fab fa-github text-lg mr-1"></i>
+                  <span>Help With a Star</span>
+                </a>
+              </div>
+              <div className="text-center mt-16"></div>
+            </div>
+          </div>
+        </div>
+      <Footer />
+    </>
+  );
+}
+
 
 async function getData(url) {
   try {
@@ -125,7 +186,7 @@ function Counter() {
 function Item({ name, isDone }) {
   return (
     <li className="item">
-      {name} {isDone ? '✅' : '❌'}
+      {name} {isDone ? <FontAwesomeIcon icon={faCheck} /> : '❌'}
     </li>
   );
 }
@@ -244,6 +305,14 @@ function Card({ children }) {
     </div>
   );
 }
+ 
+function Angular({ alt }) {
+  return <Image src={`/avatars/angular.jpg`} alt={alt}  width="64" height="64" />
+}
+ 
+function ReturnAngular() {
+  return <Angular alt="A portrait" />
+}
 
 
 export default function ToDoList() {
@@ -251,10 +320,9 @@ export default function ToDoList() {
     // only one parent allowed in jsx
     // <Item name="Nope" isDone="false"/> // example of bad way to do what's done below 
     <div>
-    <Blog/>
-     <Item isDone={true} name="Get Dockerfile sorta working with local dev" />
-     <Item isDone={true} name="Own a home"/>
+     <Index/>
      <Card>
+      <ReturnAngular/>
       <Avatar
         size={100}
         person={{ 
@@ -267,7 +335,12 @@ export default function ToDoList() {
      <MySearch/>
      <Counter/>
      <ColorBagdes/>
+     <Item isDone={true} name="Get Dockerfile sorta working with local dev" />
+     <Item isDone={true} name="Own a home"/>
+     <Item isDone={false} name="make a real index page"/>
+     <Item isDone={false} name="more routes"/>
     </div>
   );
 }
+
 
