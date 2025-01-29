7312bb8141bbfdbe865e613b9f8e6b4265889209
rjpcasalino <ryan@rjpc.net>
2025-01-28 19:11:56 -0800

    tailwinds css

--- TP/mern-app/my-next-app/app/page.tsx
+++ TP/mern-app/my-next-app/app/page.tsx
@@ -17,6 +17,37 @@ async function getData(url) {
   }
 }
 
+function ColorBagdes() {
+  return (
+    <>
+      <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-gray-500/10 ring-inset">
+        Badge
+      </span>
+      <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-red-600/10 ring-inset">
+        Badge
+      </span>
+      <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-yellow-600/20 ring-inset">
+        Badge
+      </span>
+      <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">
+        Badge
+      </span>
+      <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-blue-700/10 ring-inset">
+        Badge
+      </span>
+      <span className="inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-indigo-700/10 ring-inset">
+        Badge
+      </span>
+      <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-purple-700/10 ring-inset">
+        Badge
+      </span>
+      <span className="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-pink-700/10 ring-inset">
+        Badge
+      </span>
+    </>
+  )
+}
+
 function Counter() {
   const [data, setData] = useState(null);
   const [count, setCount] = useState(0);
@@ -175,7 +206,7 @@ function Post({ title, body }) {
 }
 
 function PostTitle({ title }) {
-  return <h1>{title}</h1>
+  return  <h1 className="text-3xl font-bold underline">{title}</h1>
 }
 
 function PostBody({ body }) {
@@ -235,6 +266,7 @@ export default function ToDoList() {
      <MyForm />
      <MySearch/>
      <Counter/>
+     <ColorBagdes/>
     </div>
   );
 }
