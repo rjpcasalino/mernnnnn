70e5fd83c409d0f265e8332d403b8a130444390b
rjpc <ryan@rjpc.net>
2025-03-15 21:37:15 -0700

    get the offical example working

---
+++ mern-app/my-next-app/app/pagez/components/ExamplePopover.tsx
@@ -0,0 +1,9 @@
+import Anchor from "react-bootstrap/Anchor";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
+
+const ExamplePopOver: React.FC = () => {
+  return ""
+};
+
+export default ExamplePopOver;
